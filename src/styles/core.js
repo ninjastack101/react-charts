98655d01bfac3e2b3c1dd9cf382fab4d56d8117c
ninjastack <ninjastack101@gmail.com>
2019-01-27 14:31:41 +0530

    Add more style type to core styles

--- src/styles/core.js
+++ src/styles/core.js
@@ -8,3 +8,9 @@ export const fullSizeView = css`
   width: 100%;
   height: 100%;
 `;
+
+export const flexCenteredView = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;

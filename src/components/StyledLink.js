81e9123eba29a6e36748d9a086644eebca954b43
ninjastack <ninjastack101@gmail.com>
2019-01-27 14:22:17 +0530

    Extracted StyledLink as separate component

---
+++ src/components/StyledLink.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-size: 17px;
+  color: white;
+`;
+
+export default StyledLink;

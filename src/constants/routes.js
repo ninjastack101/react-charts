88d7812bca503270e8dd59ff431dbd5df1bcdfe4
ninjastack <ninjastack101@gmail.com>
2019-01-30 17:58:23 +0530

    Updated constants in routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,3 +1,6 @@
-const BASE_ROUTE = '/';
 
-export default BASE_ROUTE;
+// Base path of the application.
+export const BASE_ROUTE = '/';
+
+// Path with slug where slug be the slug of a project.
+export const BASE_ROUTE_WITH_SLUG = '/:slug';
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import BASE_ROUTE from '../constants/routes';
+import { BASE_ROUTE, BASE_ROUTE_WITH_SLUG } from '../constants/routes';
 import Home from '../containers/Home';
 import RootWrapper from '../components/RootWrapper';
 
@@ -8,7 +8,7 @@ export default (
   <BrowserRouter>
     <RootWrapper>
       <Route path={BASE_ROUTE} exact component={Home} />
-      <Route path={`${BASE_ROUTE}:slug`} exact component={Home} />
+      <Route path={BASE_ROUTE_WITH_SLUG} exact component={Home} />
     </RootWrapper>
   </BrowserRouter>
 );

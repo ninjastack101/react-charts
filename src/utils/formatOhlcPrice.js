32ba07118dd4ff17043b926950715ef9aeefdd73
ninjastack <ninjastack101@gmail.com>
2019-01-30 17:31:51 +0530

    Added documentation for helper function

--- src/utils/formatOhlcPrice.js
+++ src/utils/formatOhlcPrice.js
@@ -1,10 +1,17 @@
 import moment from 'moment';
 
+/**
+ * A function that accepts prices as an array of objects, where each object
+ * has keys closePriceUsd, datetime, highPriceUsd, lowPriceUsd and openPriceUsd.
+ * 
+ * @param {Array.<Project>} projects Array of projects.
+ * @returns An array of objects with each object having additional key date
+ *          formatted in 'MMM DD' format.
+ */
 const formatOhlcPrice = prices => {
   return prices.map((price) => ({
     ...price,
     date: moment(price.datetime).format("MMM DD"),
-
   }))
 }
 
